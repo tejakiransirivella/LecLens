9ff5516627c29d080011296b30e25f98cb1b6c3f
tejakiransirivella <tejakiran2013@gmail.com>
2025-06-02 17:20:03 -0400

    docker changes

---
+++ backend/app/run.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from backend.app.api.endpoints.notes import notes_bp
+from backend.app.api.endpoints.questions import question_bp
+from backend.app.api.endpoints.quiz import quiz_bp
+from backend.app.api.endpoints.upload import upload_bp
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+# Register Blueprints
+app.register_blueprint(upload_bp)
+app.register_blueprint(question_bp)
+app.register_blueprint(quiz_bp)
+app.register_blueprint(notes_bp)
+
+if __name__ == '__main__':
+    app.run(debug=True)
